379beed44f2b5232f4ae6c1571cc741c1c74bc7b
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-12-05 10:56:41 -0300

    creo models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Perfil(models.Model):
+    nombre = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=150, blank=True)
+    resumen = models.TextField()
+    email = models.EmailField(blank=True)
+    ciudad = models.CharField(max_length=100, blank=True)
+    linkedin = models.URLField(blank=True)
+    github = models.URLField(blank=True)
+    foto = models.ImageField(upload_to='perfil/', blank=True, null=True)
+
+
+def __str__(self):
+    return self.nombre
