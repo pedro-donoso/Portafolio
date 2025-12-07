29e08d1f85c7fc09c36ad7fe4eac83b992e4bfc1
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-12-07 11:30:08 -0300

    deployado

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -5,9 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-gwz&0mfao*%fqgi6)eot*5=q5(wv+53)ftg)phvyjj#9_ho%7q'
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1',
-'localhost',
-'portafolio-2jde.onrender.com',]
+ALLOWED_HOSTS = ['portafolio-2jde.onrender.com',]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
